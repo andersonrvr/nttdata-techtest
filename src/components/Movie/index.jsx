0da68ca34d66c55536670a5240dcb39c08133bfb
Anderson Vieira <andersonrvr@gmail.com>
2023-03-09 11:45:55 -0300

    feat: final details and deploy

--- src/components/Movie/index.jsx
+++ src/components/Movie/index.jsx
@@ -1,28 +1,59 @@
 import { Button } from "@ui5/webcomponents-react";
-import { FiHeart } from "react-icons/fi";
+
 import { AiFillStar } from "react-icons/ai";
 import { useSelector } from "react-redux";
 import "./styles.scss";
+import "@ui5/webcomponents-icons/dist/heart-2.js";
+import "@ui5/webcomponents-icons/dist/heart.js";
+import "@ui5/webcomponents-icons/dist/favorite";
+import { useEffect, useState } from "react";
 
 const MovieInfo = () => {
   const { movies } = useSelector((state) => state);
-
+  const [favorited, setFavorited] = useState(false);
+  useEffect(() => {
+    setFavorited(false);
+  }, [movies]);
   return (
     <section className="movieContainer">
       {movies ? (
         <>
           <div className="infoMovie">
             <h2>{movies.Title}</h2>
-            <p>{movies.Plot}</p>
             <p>
-              <span>Actors:</span> {movies.Actors}
+              {movies.Plot === "N/A"
+                ? "No description available."
+                : movies.Plot}
+            </p>
+            <p>
+              <span>Actors:</span>{" "}
+              {movies.Actors === "N/A"
+                ? "No actors list available."
+                : movies.Actors}
             </p>
             <p className="review">
-              <span>Review:</span> <AiFillStar /> {movies.imdbRating}/10
+              <span>Review:</span>{" "}
+              {movies.imdbRating === "N/A" ? (
+                "No review available."
+              ) : (
+                <>
+                  <AiFillStar /> {movies.imdbRating}/10
+                </>
+              )}
             </p>
-            <Button style={{ fontWeight: "700" }}>
-              Favorite <FiHeart />
+            <Button
+              icon={favorited ? "heart" : "heart-2"}
+              iconEnd={true}
+              style={{
+                fontWeight: "700",
+              }}
+              onClick={() =>
+                favorited ? setFavorited(false) : setFavorited(true)
+              }
+            >
+              Favorite
             </Button>
+            {/* <FiHeart /> */}
           </div>
           <div>
             <figure>
@@ -31,7 +62,13 @@ const MovieInfo = () => {
           </div>
         </>
       ) : (
-        <div className="withoutMovie">Do your research in the input above.</div>
+        <div className="withoutMovie">
+          <p>Do your research in the input above.</p>
+          <img
+            alt="Search movie icon"
+            src="https://cdn2.iconfinder.com/data/icons/interactive-shadow-pack/100/_-8-512.png"
+          />
+        </div>
       )}
     </section>
   );
--- src/store/modules/movies/reducer.js
+++ src/store/modules/movies/reducer.js
@@ -3,7 +3,6 @@ import { GET_MOVIES } from "./actionTypes";
 const moviesReducer = (state = null, action) => {
   switch (action.type) {
     case GET_MOVIES:
-      console.log(action);
       return action.movie;
     default:
       return state;
